4efd794546bde4fd0c41afdccb55288522e12360
Carlos Matecki <carlosmatecki2@gmail.com>
2023-12-19 22:19:18 -0500

    trancitions in menu

--- Assets/Scripts/Alien/PacientBehaivor.cs
+++ Assets/Scripts/Alien/PacientBehaivor.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PacientBehaivor : MonoBehaviour
 {
     public Animator animator;
+    
     void Start()
     {
         
--- Assets/Scripts/Menu/MainMenuController.cs
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using Michsky.UI.ModernUIPack;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator animator;
+    public CinemachineVirtualCamera mainCamera;
+    public CinemachineVirtualCamera selectCamera;
+    public CinemachineVirtualCamera enterCamera;
+    
+    
+    public ButtonManagerWithIcon startButton;
+    public ButtonManagerWithIcon creditsButton;
+    public ButtonManagerWithIcon exitButton;
+    
+    
+    public GameObject book;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    public void StartGame()
+    {
+        startButton.gameObject.SetActive(false);
+        creditsButton.gameObject.SetActive(false);
+        exitButton.gameObject.SetActive(false);
+        book.GetComponent<Animator>().Play("OpenBook");
+        mainCamera.Priority = 0;
+        selectCamera.Priority = 1;
+        enterCamera.Priority = 0;
+    }
+
+    public void EnterGame()
+    {
+        mainCamera.Priority = 0;
+        selectCamera.Priority = 0;
+        enterCamera.Priority = 1;
+    }
+
+    public void OnCameraEntered()
+    {
+
+        StartCoroutine(LoadScene());
+    }
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("OutdoorsScene", LoadSceneMode.Additive);
     }
 }
